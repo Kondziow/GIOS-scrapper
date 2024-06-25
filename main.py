54ba107184ade29fd99756e29f8f47005d7babf5
Kondziow <konradwojtanowski02@gmail.com>
2024-06-25 12:04:30 +0200

    refactor: add send_get_request to avoid code repetition

--- main.py
+++ main.py
@@ -2,28 +2,29 @@ import requests
 
 base_url = "https://api.gios.gov.pl/pjp-api/rest/station"
 
-def get_all_station():
-    url = base_url + "/findAll"
+
+def send_get_request(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
         return response.json()
     except requests.RequestException as e:
-        raise Exception(f"ERROR FETCHING STATIONS: {e}")
+        raise Exception(f"HTTP request error: {e}")
+
+
+def get_all_stations():
+    url = base_url + "/findAll"
+    return send_get_request(url)
+
 
 def get_installation_by_station_id(station_id):
     url = base_url + "/sensors/" + station_id
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        return response.json()
-    except requests.RequestException as e:
-        raise Exception(f"ERROR FETCHING INSTALLATIONS FOR STATION {station_id}: {e}")
+    return send_get_request(url)
 
 
 def main():
     try:
-        stations = get_all_station()
+        stations = get_all_stations()
 
         for station in stations:
             station_id = str(station['id'])
